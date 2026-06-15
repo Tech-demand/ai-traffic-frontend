00dc4ae9dceab01be13de953fc69e447af7fd6c1
Rahul Sharma <rahulsharma274702@gmail.com>
2026-06-15 07:35:47 +0530

    Updated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import "./app.css";
+import "./App.css";
 
 import Sidebar        from "./components/Sidebar";
 import Navbar         from "./components/Navbar";
